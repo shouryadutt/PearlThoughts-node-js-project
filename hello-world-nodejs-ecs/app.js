0bc20f380210c6d70967790af4bc8341d226e15c
shouryadutt <112843008+shouryadutt@users.noreply.github.com>
2024-05-17 18:06:09 +0530

    Update app.js

--- hello-world-nodejs-ecs/app.js
+++ hello-world-nodejs-ecs/app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`App running on http://localhost:${port}`);
+  console.log(`App running on http://localhost:${3000}`);
 });
